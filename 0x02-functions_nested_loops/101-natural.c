aa826c5807fa11489cc8f5b9832e98bc70a86f19
Perezide <favourkonboye@gmail.com>
2023-07-19 16:13:22 -0700

    hello

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -9,15 +9,15 @@ int main(void)
 
 {
 	int i, sum = 0;
-	
-	while(i < 1024)
+
+	while (i < 1024)
 	{
-		if ((i % 3 == 0 ) || (i % 5 == 0))
+		if ((i % 3 == 0) || (i % 5 == 0))
 		{
 			sum += i;
 		}
 		i++;
 	}
 	printf("%d\n", sum);
-        return (0);
+	return (0);
 }
