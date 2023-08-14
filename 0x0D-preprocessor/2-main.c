ee77b15b8a9bfb873cbc04346b7f301f0d68df3a
Perezide <favourkonboye@gmail.com>
2023-08-14 07:17:58 -0700

    Correction on task 2

--- 0x0D-preprocessor/2-main.c
+++ 0x0D-preprocessor/2-main.c
@@ -1,6 +1,10 @@
 #include <stdio.h>
-
-int main() {
-	printf("This program was compiled from the file: %s\n", __FILE__);
+/**
+ * main - prints the name of the file
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void) {
+	printf("%s\n", __FILE__);
 	return 0;
 }
